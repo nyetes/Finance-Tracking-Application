ec9aeb69a84497541a8e43727b5fdbd4a9f9a755
Neetes <55584995+nyetes@users.noreply.github.com>
2023-09-08 15:27:29 +0545

    add: model changes (#1)

--- backend/models/ExpenseModel.js
+++ backend/models/ExpenseModel.js
@@ -1,39 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const ExpenseSchema = new mongoose.Schema({
+// yeta hami git ko session ledai xam.
+const ExpenseSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
     },
     amount: {
-        type: Number,
-        required: true,
-        maxLength: 20,
-        trim: true
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
     },
     type: {
-        type: String,
-        default:"expense"
+      type: String,
+      default: "expense",
     },
     date: {
-        type: Date,
-        required: true,
-        trim: true
+      type: Date,
+      required: true,
+      trim: true,
     },
     category: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true,
-        maxLength: 20,
-        trim: true
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
     },
-}, {timestamps: true})
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Expense', ExpenseSchema)
+module.exports = mongoose.model("Expense", ExpenseSchema);
